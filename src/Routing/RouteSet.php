ecf44955d22a59afbfa4267e2686fccb673092f0
paulsnar <paulsnar@paulsnar.lv>
2020-06-10 20:50:35 +0300

    Trash old routing

--- src/Routing/RouteSet.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-namespace PN\Yaf\Routing;
-
-interface RouteSet
-{
-  public function routes(RouteCollector $r): void;
-  public function globalMiddleware(): array;
-}
