3650da257333133da0854b91dc0a4587a8eb8e30
paulsnar <paulsnar@paulsnar.lv>
2020-06-13 15:28:17 +0300

    Change RouteSet interface signature also
    Whoops, refactoring went poorly.

--- src/Routing/RouteSet.php
+++ src/Routing/RouteSet.php
@@ -3,6 +3,5 @@ namespace PN\Yaf\Routing;
 
 interface RouteSet
 {
-  public function routes(RouteCollector $r): void;
-  public function globalMiddleware(): array;
+  public function draw(RouteCollector $r): void;
 }
